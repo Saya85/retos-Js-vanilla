10dda786c7f8e260f790da45303cea9fe1362425
Saya85 <95279028+Saya85@users.noreply.github.com>
2021-12-16 17:19:05 +0100

    reto 1  terminado y funcional

---
+++ Js/funcion.js
@@ -0,0 +1,16 @@
+
+
+function mayorIgual(num1, num2) {
+    if (num1 < num2) {
+        alert('el segundo numero es mayor')
+    }else if(num1 > num2){
+        alert('el primer numero es mayor')
+    }else{
+        alert('los dos numeros son iguales')
+    }
+}
+   
+
+function bienvenido(params) {
+    
+}
---
+++ reto1/Js/main.js
@@ -0,0 +1,7 @@
+
+
+
+let num1= prompt('escribe el primer numero');
+let num2= prompt('escribe el segundo numero');
+//funcion de mayor o igual que.
+mayorIgual(num1,num2);
